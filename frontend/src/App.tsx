2a10bd5a6813fd63eef195e5adb84fbddbd212e9
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-01-15 00:35:16 +0100

    config frontu-1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from  'react'
+import WelcomePage from "./components/WelcomePage";
+
+function App() {
+
+  return (
+    <div className="App">
+      <WelcomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/WelcomePage.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from "react";
+
+const WelcomePage: FC = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+ 
+export default WelcomePage;
