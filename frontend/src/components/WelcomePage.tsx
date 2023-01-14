277e35a842e8386e0c5989bca306746ef4c2312b
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-01-15 00:43:05 +0100

    wrk in prgs

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -3,7 +3,14 @@ import React, {FC} from "react";
 const WelcomePage: FC = () => {
   return (
     <div>
-        
+      <div className="">
+
+      </div>
+      <div>
+        <button>
+          zarejestruj się
+        </button>
+      </div>
     </div>
   );
 };
