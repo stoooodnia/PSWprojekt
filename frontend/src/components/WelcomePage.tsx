96c38730afb57b9a5a865ff15c83ef6ce33e3c83
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-01-19 17:28:38 +0100

    work in progress

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -1,10 +1,46 @@
 import React, {FC} from "react";
+import { Resolver, useForm } from "react-hook-form";
+import sha256 from "fast-sha256"
+
+
+type FormValues = {
+  email: string;
+  password: string;
+}
+
+// const resolver: Resolver<FormValues> = async (values) => {
+//   return {
+//     values: values.firstName ? values : {},
+//     errors: !values.firstName
+//       ? {
+//           firstName: {
+//             type: 'required',
+//             message: 'This is required.',
+//           },
+//         }
+//       : {},
+//   };
+// };
 
 const WelcomePage: FC = () => {
+  
+
+  const { register, handleSubmit, formState: {errors} } = useForm<FormValues>();
+
+
+  const onSubmit = (data: any) => {
+    const password = sha256(data.password)
+  };
   return (
     <div>
+      <h1 className="text-l">Tajniacy</h1>
       <div className="">
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} placeholder="email"/>
 
+      <input {...register("password")} placeholder="password" />
+      <button type="submit">zaloguj</button>
+    </form>
       </div>
       <div>
         <button>
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      black: "#2d2a32",
+      white: "#f7f9f9",
+      gray: "#464f51",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
