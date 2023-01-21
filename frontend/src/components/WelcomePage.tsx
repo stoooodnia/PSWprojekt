ebf7a3468d488ce2d52befac867adacf70324f30
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-01-21 22:54:20 +0100

    nie wiem ocb

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -1,12 +1,11 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import { Resolver, useForm } from "react-hook-form";
-import sha256 from "fast-sha256"
-
+import sha256 from "fast-sha256";
 
 type FormValues = {
   email: string;
   password: string;
-}
+};
 
 // const resolver: Resolver<FormValues> = async (values) => {
 //   return {
@@ -23,32 +22,35 @@ type FormValues = {
 // };
 
 const WelcomePage: FC = () => {
-  
-
-  const { register, handleSubmit, formState: {errors} } = useForm<FormValues>();
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
 
   const onSubmit = (data: any) => {
-    const password = sha256(data.password)
+    const password = sha256(data.password);
   };
   return (
-    <div>
-      <h1 className="text-l">Tajniacy</h1>
-      <div className="">
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("email")} placeholder="email"/>
-
-      <input {...register("password")} placeholder="password" />
-      <button type="submit">zaloguj</button>
-    </form>
+    <div className="">
+      <div id="spy" className="bg-spy h-full w-1/2">
+        nie wchodzi mi obrazek
       </div>
-      <div>
-        <button>
-          zarejestruj się
-        </button>
+      <div className="flex flex-col flex-end justify-center items-center h-full">
+        <h1 className="text-l h-1/4">Tajniacy</h1>
+        <div className="">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("email")} placeholder="email" />
+            <input {...register("password")} placeholder="password" />
+            <button type="submit">zaloguj</button>
+          </form>
+        </div>
+        <div>
+          <button>zarejestruj się</button>
+        </div>
       </div>
     </div>
   );
 };
- 
+
 export default WelcomePage;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    colors: {
-      black: "#2d2a32",
-      white: "#f7f9f9",
-      gray: "#464f51",
+    extend: {
+      colors: {
+        'black': "#2d2a32",
+        'white': "#f7f9f9",
+        'gray': "#464f51",
+      },
+      backgroundImage: {
+        'spy': "url('./src/assets/images/spy.jpg')",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
