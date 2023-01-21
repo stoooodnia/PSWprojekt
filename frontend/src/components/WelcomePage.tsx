c1d41d0ed7f59d5bda48749426da4e9b78199589
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2023-01-22 00:08:51 +0100

    wreszcie layout aaaaaa

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -32,21 +32,39 @@ const WelcomePage: FC = () => {
     const password = sha256(data.password);
   };
   return (
-    <div className="">
-      <div id="spy" className="bg-spy h-full w-1/2">
-        nie wchodzi mi obrazek
-      </div>
-      <div className="flex flex-col flex-end justify-center items-center h-full">
-        <h1 className="text-l h-1/4">Tajniacy</h1>
-        <div className="">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email")} placeholder="email" />
-            <input {...register("password")} placeholder="password" />
-            <button type="submit">zaloguj</button>
+    <div className="flex flex-row flex-end h-screen w-screen">
+      <div id="spy" className="flex h-full w-1/2" />
+      <div className="flex flex-col flex-end justify-center items-center h-full w-1/2 gap-2">
+        <h1 className="text-7xl h-1/4">TAJNIACY</h1>
+        <div className="w-full">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col justify-center items-center gap-8"
+          >
+            <div className="flex justify-center w-full gap-10">
+              <input
+                {...register("email")}
+                placeholder="email"
+                className="w-56 text-center bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-myWhite focus:border-myBlack"
+              />
+              <input
+                {...register("password")}
+                placeholder="password"
+                className="w-56 text-center bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-myWhite focus:border-myBlack"
+              />
+            </div>
+            <button
+              type="submit"
+              className=" w-24 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
+              zaloguj
+            </button>
           </form>
         </div>
         <div>
-          <button>zarejestruj się</button>
+          <button className="inline-block align-baseline font-bold text-sm text-pink-500 hover:text-pink-800">
+            zarejestruj się
+          </button>
         </div>
       </div>
     </div>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'black': "#2d2a32",
-        'white': "#f7f9f9",
-        'gray': "#464f51",
-      },
-      backgroundImage: {
-        'spy': "url('./src/assets/images/spy.jpg')",
+        myBlack: "#2d2a32",
+        myWhite: "#f7f9f9",
+        myGray: "#464f51",
       },
     },
   },
